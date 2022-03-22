ce1b2216f64ac0a7acf9e7ef92dfbde40fda3e7a
OwlAtom <57601828+OwlAtom@users.noreply.github.com>
2022-03-23 00:15:08 +0100

    remove alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,18 +75,3 @@ range.oninput = (v) => {
   document.querySelector("#rangeViz").innerText =
     "Range in meters: " + range.value * 10;
 };
-
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    alert(
-      "Lat: " + position.coords.latitude + "\nLon: " + position.coords.longitude
-    );
-  },
-  function (error) {
-    alert(error.message);
-  },
-  {
-    enableHighAccuracy: true,
-    timeout: 5000,
-  }
-);
